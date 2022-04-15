2eadb57fdda1ea8dafde636c0c39be2180cbcb17
aehler <aehler@yandex.ru>
2022-04-15 12:31:26 +0300

    Update config.go
    1

--- lib/config/config.go
+++ lib/config/config.go
@@ -12,13 +12,3 @@ type RestCreds struct {
 	Password string `json:"password"`
 }
 
-var Data = Config{
-	KafkaAddressPool: []string{"10.204.192.111:9192","10.204.192.112:9192","10.204.192.113:9192"},
-	RestAddress: "https://eaist2-f.mos.ru/module/uas-2/api/v1",
-	RestCreds :RestCreds{
-		ExtSystem: "DIT_test_http",
-		Login: "DIT_test_http",
-		Password: "DIT_test_http",
-	},
-
-}
